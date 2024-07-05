cd65f492930a941eb105bc861ba76e51e075903a
H.A Raihan <92677206+raihanhan@users.noreply.github.com>
2024-07-05 19:46:55 +0700

    Update LivePage.jsx

--- src/pages/LivePage/LivePage.jsx
+++ src/pages/LivePage/LivePage.jsx
@@ -47,7 +47,7 @@ const LivePage = () => {
                 <iframe
                   width="100%"
                   height="500"
-                  src={events.liveSrc}
+                  src={events.liveUrl}
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
